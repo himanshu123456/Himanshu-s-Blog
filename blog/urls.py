7c7cb649d896c595d30f5bba766bf3ee1a80f38e
himanshu123456 <himanshu.deshwal@yahoo.com>
2015-07-22 20:27:01 +0530

    post_details is visible

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-url(r'^$',views.post_list,name='post_list'),]
+url(r'^$',views.post_list,name='post_list'),
+url(r'^post/(?P<pid>[0-9]+)/$', views.post_details, name='post_details'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.utils import timezone
 def post_list(request):
 	post=Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
 	return  render(request,'blog/post_list.html',{'posts':post})
+
+def post_details(request,pid):
+	post=get_object_or_404(Post,pk=pid)
+	return render(request,'blog/post_details.html',{'post':post})
